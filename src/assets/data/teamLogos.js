ef1b25ab44bb595efc908a844c18576d8b4b4ca1
MeGallin <me@garyallin.uk>
2023-03-30 12:10:58 +0100

    setup

---
+++ src/assets/data/teamLogos.js
@@ -0,0 +1,15 @@
+export const teamLogos = [
+  { team: 'Aston Martin', path: '../../assets/teamLogos/aston-martin-2.svg' },
+  { team: 'Mercedes', path: '../../assets/teamLogos/mercedes-amg-logo.svg' },
+  { team: 'Ferrari', path: '../../assets/teamLogos/ferrari-ges.svg' },
+  { team: 'Alfa Romeo', path: '../../assets/teamLogos/alfa-romeo-4.svg' },
+  { team: 'Alpine', path: '../../assets/teamLogos/alpine-5.svg' },
+  { team: 'Williams', path: '../../assets/teamLogos/williams-racing-1.svg' },
+  { team: 'AlphaTauri', path: '../../assets/teamLogos/alphatauri.svg' },
+  {
+    team: 'Haas F1 Team',
+    path: '../../assets/teamLogos/haas-f1-team-logo.svg',
+  },
+  { team: 'McLaren', path: '../../assets/teamLogos/mclaren-f1-2.svg' },
+  { team: 'Red Bull Racing', path: '../../assets/teamLogos/redbull-1.svg' },
+];
