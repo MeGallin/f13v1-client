250fc85b3b35ed80185eea7287c33a77cf4ebdec
MeGallin <me@garyallin.uk>
2023-04-14 15:37:04 +0100

    minor tweaks

--- src/Components/Navigation/NavigationComponent.jsx
+++ src/Components/Navigation/NavigationComponent.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavigationComponent.css';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./NavigationComponent.css";
 
 const NavigationComponent = () => {
   const [toggleHamburger, setToggleHamburger] = useState(false);
@@ -10,24 +10,17 @@ const NavigationComponent = () => {
   return (
     <>
       <nav>
-        <NavLink
-          onClick={handleNavigation}
-          className={(navData) => (navData.isActive ? 'active' : '')}
-          to="/"
-        >
-          LOGO
-        </NavLink>
         <div className="hamburger" onClick={handleNavigation}>
           <div className="line"></div>
           <div className="line"></div>
           <div className="line"></div>
         </div>
 
-        <ul className={!toggleHamburger ? 'nav_links open' : 'nav_links'}>
+        <ul className={!toggleHamburger ? "nav_links open" : "nav_links"}>
           <li className="link">
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/"
               data-cy="nav-home-link"
             >
@@ -35,7 +28,7 @@ const NavigationComponent = () => {
             </NavLink>
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/standings"
               data-cy="nav-standings-link"
             >
@@ -43,7 +36,7 @@ const NavigationComponent = () => {
             </NavLink>
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/calendar"
               data-cy="nav-calendar-link"
             >
@@ -52,7 +45,7 @@ const NavigationComponent = () => {
 
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/drivers"
               data-cy="nav-drivers-link"
             >
@@ -61,21 +54,21 @@ const NavigationComponent = () => {
 
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/round-up"
               data-cy="nav-roundUp-link"
             >
               SPORT
             </NavLink>
-
+            {/* 
             <NavLink
               onClick={handleNavigation}
-              className={(navData) => (navData.isActive ? 'active' : '')}
+              className={(navData) => (navData.isActive ? "active" : "")}
               to="/about"
               data-cy="nav-about-link"
             >
               about
-            </NavLink>
+            </NavLink> */}
           </li>
         </ul>
       </nav>
